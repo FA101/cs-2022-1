ef8344700f3022a46adbd55358adb6b91a8fef6d
Fillipe <fillipe.albuquerque@discente.ufg.br>
2022-07-07 00:21:48 -0300

    Atualiza Aula10

--- Aula10/Recursividade/src/main/java/quicksortrec/Teste.java
+++ Aula10/Recursividade/src/main/java/quicksortrec/Teste.java
@@ -8,46 +8,46 @@ public class Teste {
     private static final int T2 = 10000;
     public static void main(String[] args) {    
         
-        int vetor[] = new int[T];
-        int vetor1[] = new int[T1];
-        int vetor2[] = new int[T2];
+        int[] vetor = new int[T];
+        int[] vetor1 = new int[T1];
+        int[] vetor2 = new int[T2];
 
         for(int i=0;i<T;i++){
             vetor[i]= aleatorio();
         }
 
         for(int i1=0;i1<T1;i1++){
-            vetor1[i1]= aleatorio();
+            vetor1[i1]= aleatorio1();
         }
 
-        for(int i2=0;i2<T;i2++){
-            vetor2[i2]= aleatorio();
+        for(int i2=0;i2<T2;i2++){
+            vetor2[i2]= aleatorio2();
         }
 
         long inicio = System.currentTimeMillis();        
-        long inicio1 = System.currentTimeMillis();        
-        long inicio2 = System.currentTimeMillis();        
-
-        QuickSortRecursivo sort = new QuickSortRecursivo();              
-        QuickSortRecursivo sort1 = new QuickSortRecursivo();   
-        QuickSortRecursivo sort2 = new QuickSortRecursivo();
-
+        QuickSortRecursivo sort = new QuickSortRecursivo(); 
         sort.ordenarVetorDeInteiros(vetor);
-        sort1.ordenarVetorDeInteiros(vetor1);
-        sort2.ordenarVetorDeInteiros(vetor2);
-
         long fim = System.currentTimeMillis();
-        long fim1 = System.currentTimeMillis();
-        long fim2 = System.currentTimeMillis();
-
         long tempo = (fim - inicio);  
+
+        long inicio1 = System.currentTimeMillis();                
+        QuickSortRecursivo sort1 = new QuickSortRecursivo();           
+        sort1.ordenarVetorDeInteiros(vetor1);
+        long fim1 = System.currentTimeMillis();
         long tempo1 = (fim1 - inicio1);  
+
+        long inicio2 = System.currentTimeMillis();        
+        QuickSortRecursivo sort2 = new QuickSortRecursivo();
+        sort2.ordenarVetorDeInteiros(vetor2);
+        long fim2 = System.currentTimeMillis();        
         long tempo2 = (fim2 - inicio2);  
 
+        /**
         imprimirVetor(vetor);
         imprimirVetor(vetor1);
         imprimirVetor(vetor2);
-
+        */
+        
         System.out.println("\n");
         System.out.println(tempo);
         
@@ -58,18 +58,30 @@ public class Teste {
         System.out.println(tempo2);
     }
 
+    /**
     private static void imprimirVetor(int[] vetor) {
         System.out.println("Vetor...\n");
         for(int num : vetor) {
             System.out.print(num + ", ");
         }
     }
+    */
 
     public static int aleatorio(){
         Random rand = new Random(); 
         int upperbound = T;             
-        int int_random = rand.nextInt(upperbound);                     
-        return int_random;            
+        return rand.nextInt(upperbound);            
     }
     
+    public static int aleatorio1(){
+        Random rand1 = new Random(); 
+        int upperbound1 = T1;             
+        return rand1.nextInt(upperbound1);            
+    }
+
+    public static int aleatorio2(){
+        Random rand2 = new Random(); 
+        int upperbound2 = T2;             
+        return rand2.nextInt(upperbound2);            
+    }
 }
